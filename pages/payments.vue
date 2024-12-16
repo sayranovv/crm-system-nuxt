a773a65fcd6ee5892e62b6ec1de95a60441ff3ca
Eldar Sayranov <sayranoveldar@gmail.com>
2024-12-16 14:20:51 +0500

    edit pages #2

--- pages/payments.vue
+++ pages/payments.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="p-10">
-    <h1 class="font-bold text-2xl mb-10">Оплата</h1>
+    <h1 class="font-bold text-2xl mb-10">Платежи</h1>
   </div>
 </template>
--- pages/products.vue
+++ pages/products.vue
@@ -1,3 +1,5 @@
 <template>
-	<div></div>
+  <div class="p-10">
+    <h1 class="font-bold text-2xl mb-10">Товары</h1>
+  </div>
 </template>
